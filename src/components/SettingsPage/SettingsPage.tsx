66d57ead831b371159e2bac46963fddd41e3985a
falkinshack <falkinshack@proton.me>
2024-07-21 16:00:33 +0200

    Implement thermocontrol data fetching and settings for refresh interval

--- src/components/SettingsPage/SettingsPage.tsx
+++ src/components/SettingsPage/SettingsPage.tsx
@@ -1,13 +1,57 @@
-import { Box } from "@chakra-ui/react";
+import { Button, Divider, HStack, Input, Text, useNumberInput, VStack } from "@chakra-ui/react";
+import useLocalStorage from "use-local-storage";
+
+const MIN_REFRESH_INTERVAL = 100;
+const MAX_REFRESH_INTERVAL = 3600000;
+const STEP_REFRESH_INTERVAL = 500;
 
 function SettingsPage() {
 
+    const [refreshInterval, setRefreshInterval] = useLocalStorage('refreshInterval',5000);
+
+    function enforceRefreshRange(inputValue: number){
+        if(inputValue < MIN_REFRESH_INTERVAL){
+        setRefreshInterval(MIN_REFRESH_INTERVAL);}
+        else{
+            if(inputValue > MAX_REFRESH_INTERVAL){
+                setRefreshInterval(MAX_REFRESH_INTERVAL);
+            }
+            setRefreshInterval(inputValue);
+        }
+    }
+
+    const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } =
+        useNumberInput({
+            step: STEP_REFRESH_INTERVAL,
+            value: refreshInterval,
+            onChange: (_valueString, valueAsNumber) => enforceRefreshRange(valueAsNumber),
+            min: MIN_REFRESH_INTERVAL,
+            max: MAX_REFRESH_INTERVAL,
+            precision: 0,
+        });
+
+
+    function IntervalInput() {
+        const inc = getIncrementButtonProps();
+        const dec = getDecrementButtonProps();
+        const input = getInputProps();
+
+        return (
+            <HStack maxW='200px'>
+                <Button {...dec}>-</Button>
+                <Input {...input} />
+                <Button {...inc}>+</Button>
+            </HStack>
+        );
+    }
 
     return (
-        <Box as="main" flex="1" p={4} bg="lightgreen" width="100%">
-            Settings
-        </Box>
-    )
+        <VStack>
+            <Text>Refresh interval [ms]</Text>
+            <IntervalInput />
+            <Divider />
+        </VStack>
+    );
 }
 
 export default SettingsPage;
--- src/components/ThermocontrolPage/ThermocontrolDetails.tsx
+++ src/components/ThermocontrolPage/ThermocontrolDetails.tsx
@@ -1,6 +1,57 @@
 import { Box, Button, Divider, HStack, Input, Slider, SliderFilledTrack, SliderThumb, SliderTrack, Switch, Text, useNumberInput, VStack } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import useLocalStorage from "use-local-storage";
+
+interface ThermocontrolDataType {
+    emergency_heating_is_active: boolean;
+    extra_ventilation: number;
+    max_heating_power: number;
+    sensor_data_age: number;
+    target_humidity: number;
+    target_temperature: number;
+    use_ventilation_for_cooling: boolean;
+    use_ventilation_for_heating: boolean;
+}
 
 function ThermocontrolDetails() {
+
+    const [refreshInterval] = useLocalStorage('refreshInterval',5000);
+    const [data, setData] = useState<ThermocontrolDataType | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchDataForPosts = async () => {
+            try {
+                const postsData = await axios.get<ThermocontrolDataType>(
+                    'http://10.0.2.2:9079/json'
+                );
+                //console.log(postsData)
+                setData(postsData.data);
+                setError(null);
+            }
+            catch (error) {
+                if (axios.isAxiosError(error)) {
+                    setError(error.message);
+                } else {
+                    setError("error");
+                }
+            }
+            finally {
+                setLoading(false);
+            }
+        };
+
+        fetchDataForPosts();
+
+        const interval=setInterval(()=>{
+            fetchDataForPosts()
+            },refreshInterval)
+       
+       
+           return()=>clearInterval(interval)
+    }, []);
 
     function TemperatureInput() {
         const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } =
@@ -19,7 +70,7 @@ function ThermocontrolDetails() {
         return (
             <HStack maxW='200px'>
                 <Button {...dec}>-</Button>
-                <Input {...input} />
+                <Input {...input} value={data?.target_temperature}/>
                 <Button {...inc}>+</Button>
             </HStack>
         )
@@ -42,14 +93,14 @@ function ThermocontrolDetails() {
         return (
             <HStack maxW='200px'>
                 <Button {...dec}>-</Button>
-                <Input {...input} />
+                <Input {...input} value={data?.target_humidity}/>
                 <Button {...inc}>+</Button>
             </HStack>
         )
     }
 
     return (
-        <Box width={"fit-content"} border={"2px"} p={2}>
+        <Box width={"fit-content"} border={"2px"} borderColor={loading? "orange" : error? "red" : "green"} p={2}>
             <VStack>
                 <Text>Target Temperature</Text>
                 <TemperatureInput></TemperatureInput>
@@ -58,7 +109,7 @@ function ThermocontrolDetails() {
                 <HumidityInput></HumidityInput>
                 <Divider></Divider>
                 <Text>Extra ventilation</Text>
-                <Slider defaultValue={1} min={0} max={1} step={0.1}>
+                <Slider defaultValue={1} min={0} max={1} step={0.1} value={data?.extra_ventilation}>
                     <SliderTrack >
                         <SliderFilledTrack />
                     </SliderTrack>
@@ -66,7 +117,7 @@ function ThermocontrolDetails() {
                 </Slider>
                 <Divider></Divider>
                 <Text>Max heating power</Text>
-                <Slider defaultValue={1} min={0} max={1} step={0.1}>
+                <Slider defaultValue={1} min={0} max={1} step={0.1} value={data?.max_heating_power}>
                     <SliderTrack >
                         <SliderFilledTrack />
                     </SliderTrack>
@@ -74,9 +125,12 @@ function ThermocontrolDetails() {
                 </Slider>
                 <Divider></Divider>
                 <Divider></Divider>
-                <Switch>Use ventilation for heating</Switch>
+                <Switch isChecked={data?.use_ventilation_for_heating}>Use ventilation for heating</Switch>
                 <Divider></Divider>
-                <Switch>Use ventilation for cooling</Switch>
+                <Switch isChecked={data?.use_ventilation_for_cooling}>Use ventilation for cooling</Switch>
+                <Divider></Divider>
+                <Text>{"Data age: " + data?.sensor_data_age}</Text>
+                {data?.emergency_heating_is_active ? <Text>Emergency heating is active!</Text> : null}
             </VStack>
         </Box>
     )
