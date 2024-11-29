151e6dd216605ab49d996942b336700ba4620244
falkinshack <falkinshack@proton.me>
2024-11-30 00:08:55 +0100

    Add guest login feature

--- src/components/LoginPage.tsx/LoginPage.tsx
+++ src/components/LoginPage.tsx/LoginPage.tsx
@@ -12,9 +12,19 @@ const LoginPage: React.FC = () => {
     const navigate = useNavigate();
     const location = useLocation();
 
+    const handleGuestLogin = async () => {
+            const loginResult = await auth.login("guest", "");
+        if (loginResult.error) {
+            console.log(loginResult.error);
+            setError('Guest login failed. There seems to be a technical issue. Please try again later.');
+        } else {
+            const from = (location.state as { from?: Location })?.from?.pathname || "/";
+            navigate(from, { replace: true });
+        }
+    };
+
     const handleSubmit = async () => {
-        const loginResult = username.length === 0 ?
-            await auth.login("guest", "") :
+        const loginResult = 
             await auth.login(username, password);
         if (loginResult.error) {
             console.log(loginResult.error);
@@ -37,20 +47,34 @@ const LoginPage: React.FC = () => {
                 boxSize={"max(30vw, 200px)"}
                 marginTop={"5vh"}
                 alt='Shacktopus Logo' />
-            <Box minWidth={"200px"} width={"20vw"}>
-                {error && <p style={{ color: 'red' }}>{error}</p>}
-                <Text>User</Text>
-                <Input
-                    type="text"
-                    value={username} onChange={(e) => setUsername(e.target.value)}
-                    onKeyDown={handleKeyDown} />
-                <Text>Password</Text>
-                <Input
-                    type="password"
-                    value={password} onChange={(e) => setPassword(e.target.value)}
-                    onKeyDown={handleKeyDown} />
-            </Box>
-            <Button onClick={handleSubmit}>Login</Button>
+
+            <VStack
+                marginTop={"10vh"}>
+                <Button
+                onClick={handleGuestLogin}
+                >Login as Guest</Button>
+                <Text
+                    marginTop={"2vh"}
+                >
+                    or</Text>
+                <Box
+                    minWidth={"180px"}
+                    width={"18vw"}>
+                    {error && <p style={{ color: 'red' }}>{error}</p>}
+                    <Text>User</Text>
+                    <Input
+                        type="text"
+                        value={username} onChange={(e) => setUsername(e.target.value)}
+                        onKeyDown={handleKeyDown} />
+                    <Text>Password</Text>
+                    <Input
+                        type="password"
+                        value={password} onChange={(e) => setPassword(e.target.value)}
+                        onKeyDown={handleKeyDown} />
+                </Box>
+                <Button onClick={handleSubmit}>Login</Button>
+            </VStack>
+
         </VStack>
     );
 };
