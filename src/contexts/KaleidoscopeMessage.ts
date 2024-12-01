8e241138239b6e1d8149a46a50fd286aec03268f
falkinshack <falkinshack@proton.me>
2024-12-01 01:46:20 +0100

    Improve kaleidoscope api message type definition

--- src/contexts/KaleidoscopeMessage.ts
+++ src/contexts/KaleidoscopeMessage.ts
@@ -1,7 +1,10 @@
-export interface KaleidoscopeMessage {
+export type KaleidoscopeMessage = {
+    messageType: "update";
+    health: "good" | "error";
+    data: any;
+} | {
     // tokenError hints at an expired or invalid token and should be handled by the UI by logging out the user
-    messageType: "update" | "error" | "tokenError";
-    error?: string;
-    health?: "good" | "error";
-    data?: any;
+    messageType: "error" | "tokenError";
+    // should be set if message type is error or tokenError. It is a human-readable error message
+    error: string;
 };
